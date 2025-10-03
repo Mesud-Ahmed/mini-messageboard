1baacca7cb857a7abe5b5beb9441e89dbd14588c
Mesud-Ahmed <mesudahmed94@gmail.com>
2025-10-03 07:04:22 +0300

    Use process.env.PORT and ensure start script

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "public")));
 const indexRouter = require('./routes/indexRouter')
 app.use('/',indexRouter)
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
